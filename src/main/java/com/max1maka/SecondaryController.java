10fdce9ae598013cc520c12cd48638c1343582f9
Maxim Shchemelev <71179422+MaximPshonka@users.noreply.github.com>
2021-02-17 18:04:41 +0300

    initial commit

---
+++ src/main/java/com/max1maka/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.max1maka;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
