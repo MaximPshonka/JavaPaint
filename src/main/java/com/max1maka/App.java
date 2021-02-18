6fc533800c9d7a7a7085287e101f9fb3b3d64e4a
Maxim Shchemelev <71179422+MaximPshonka@users.noreply.github.com>
2021-02-18 15:32:52 +0300

    title upd, readme upd

--- src/main/java/com/max1maka/App.java
+++ src/main/java/com/max1maka/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 800, 640);
         stage.setScene(scene);
+        stage.setTitle("Paint");
         stage.setResizable(false);
         stage.show();
     }
